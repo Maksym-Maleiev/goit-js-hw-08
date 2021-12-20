21af26631090baf97f8659009fd6279bf5f18b0f
Maksym-Maleiev <maximmaleev93@gmail.com>
2021-12-20 21:38:13 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const currentTime = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+onSetCurrentTime();
+
+// function onCurrentTime({ seconds }) {
+//   localStorage.setItem(currentTime, seconds);
+// }
+
+player.getCurrentTime().then(function(seconds) {
+  localStorage.setItem(currentTime, seconds);
+  console.log(seconds)
+}).catch(function(error) {
+    console.log("error")
+});
+
+function onSetCurrentTime() {
+  if (!localStorage.getItem(currentTime)) {
+    return;
+  }
+
+  player.setCurrentTime(localStorage.getItem(currentTime));
+}
