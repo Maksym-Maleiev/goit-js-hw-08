5ddddc977da91155cad966314623108114bb7e7f
Maksym-Maleiev <maximmaleev93@gmail.com>
2021-12-27 11:00:11 +0200

    feedback fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,56 @@
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
 
-const LOCALSTORAGE_KEY = "feedback-form-state";
-const form = document.querySelector(".feedback-form");
-let formFilter = {};
+const MESSAGE_KEY = 'feedback-form-state';
+const data = {}
 
-form.addEventListener("input", throttle(inputForm, 500))
-function inputForm(evt) {
-    formFilter[evt.target.name] = evt.target.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formFilter)); 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+
+onMessageOutput();
+onSaved();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextInput, 500));
+form.addEventListener('input', throttle(onConsoleOutput, 500));
+
+function onSaved () {
+    data.email = input.value;
+    data.message = textarea.value;
 }
 
-initForm()
-function initForm() {
-  let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
-  if (persistedFilters) {
-    persistedFilters = JSON.parse(persistedFilters)
-    Object.entries(persistedFilters).forEach(([name, value]) => {
-      form.elements[name].value = value;
-    })
-  }
+function onConsoleOutput(event){
+    data[event.target.name] = event.target.value;
 }
 
-form.addEventListener("submit", saveEmail);
-function saveEmail(evt) {
-  evt.preventDefault();
+function onFormSubmit (event) {
+    event.preventDefault();
 
-  const inputEmail = form.elements.email.value;
-  const inputMessage = form.elements.message.value;
-  if (inputEmail && inputMessage !== "") {
-    let userForm = localStorage.getItem(LOCALSTORAGE_KEY);
-    userForm = JSON.parse(userForm);
-    console.log(userForm);
-    localStorage.removeItem(LOCALSTORAGE_KEY);
-  evt.currentTarget.reset();  
-  } 
-  }
+    if(!(event.currentTarget.elements.email.value === '' || event.currentTarget.elements.message.value === ''))
+    {
+        event.currentTarget.reset();
+        localStorage.removeItem(MESSAGE_KEY);
+        if(Object.keys(data).length === 0 && Object.values(data).length === 0) return;
+        console.log(data);
+        
+    } else{
+    alert('Все поля должны быть заполнены!');
+    return;}
+}
+
+
+function onTextInput (event) {
+
+
+    if(Object.keys(data).length === 0) return;
+    const dataStr = JSON.stringify(data);
+    localStorage.setItem(MESSAGE_KEY, dataStr);
+}
+
+
+function onMessageOutput () {
+    const savedData = localStorage.getItem(MESSAGE_KEY);
+    if(!savedData) return;
+    textarea.value =  JSON.parse(savedData)["message"];
+    input.value =  JSON.parse(savedData)["email"];
+}
